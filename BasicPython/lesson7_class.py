26af7ab83d454027172acc4eeeddbef389f4d20d
JuHan Park <iiiamst16@gmail.com>
2023-10-29 22:26:12 +0900

    add Ractangle class(implement Ball class)

--- BasicPython/lesson7_class.py
+++ BasicPython/lesson7_class.py
@@ -9,10 +9,10 @@ class Ball:
         self.color = color
 
     def move(self, canvas):
-        canvas.create_oval(self.x -21, self.y-21, self.x+21, self.y+21, fill="white", width=0)
+        self.erase(canvas)
         self.x += self.dx
         self.y += self.dy
-        canvas.create_oval(self.x -20, self.y-20, self.x+20, self.y+20, fill=self.color, width=0)
+        self.draw(canvas)
         
         if self.x > canvas.winfo_width() :
             self.dx = -1
@@ -23,12 +23,25 @@ class Ball:
         if self.y <= 0:
             self.y = 1
 
+    def erase(self, canvas):
+        canvas.create_oval(self.x - 21, self.y - 21, self.x + 21, self.y +21, width=0, fill="white")
+    def draw(self, canvas):
+        canvas.create_oval(self.x - 20, self.y - 20, self.x + 20, self.y + 20, width=0, fill=self.color)
+
+class Rectangle(Ball):
+    def erase(self, canvas):
+        canvas.create_rectangle(self.x - 21, self.y - 21, self.x + 21, self.y +21, width=0, fill="white")
+    def draw(self, canvas):
+        canvas.create_rectangle(self.x - 21, self.y - 21, self.x + 21, self.y +21, width=0, fill=self.color)
+
 b = Ball(400, 300, 1, 1, "red")
 c = Ball(500,200,-1,1,"Blue")
+d = Rectangle(600, 500, -1, -1, "green")
 
 def loop():
     b.move(canvas)
     c.move(canvas)
+    d.move(canvas)
     root.after(10, loop)
 
 root = tk.Tk()
