aadcf66f071bb1ae5fd26374dcb846b0c80e150a
JuHan Park <iiiamst16@gmail.com>
2023-12-05 17:11:14 +0900

    Class 2 수 찾기

---
+++ Python Algorithm solving/solved_ac/Class2.py
@@ -0,0 +1,12 @@
+# 수 찾기 1920번
+# import sys
+# N = int(input())
+# n_dict = dict.fromkeys(map(int,sys.stdin.readline().strip().split()))
+# M = int(input())
+# m_list = list(map(int,sys.stdin.readline().strip().split()))
+# for m in m_list:
+#     if m in n_dict:
+#         sys.stdout.write('1\n')
+#     else:
+#         sys.stdout.write('0\n')
+
