4f9672b5b647bf67de93344706732d5dea492333
JuHan Park <iiiamst16@gmail.com>
2023-12-06 11:59:02 +0900

    Class 2 괄호

--- Python Algorithm solving/solved_ac/Class2.py
+++ Python Algorithm solving/solved_ac/Class2.py
@@ -10,3 +10,53 @@
 #     else:
 #         sys.stdout.write('0\n')
 
+# 카드2 2164번
+
+# N = int(input())
+# n_list = [i+1 for i in range(N)]
+# while len(n_list) != 1:
+#     tmp = n_list[1::2]
+#     if len(n_list) % 2 != 0:
+#        tmp.insert(0,n_list[-1])
+#     n_list = tmp
+# print(n_list[0])
+
+#괄호 9012번
+class MyStack:
+    def __init__(self,size):
+        self.size = size
+        self.p = -1
+        self.stack = [0 for _ in range(size)]
+    
+    def push(self,v):
+        self.p += 1
+        self.stack[self.p] = v
+    
+    def pop(self):
+        if self.p == -1:
+            return -1
+        tmp = self.stack[self.p]
+        self.p -= 1
+        return tmp
+
+    def __str__(self):
+        return str(self.stack)
+
+N = int(input())
+for i in range(N):
+    stack = MyStack(50)
+    ex = input()
+    b = True
+    for p in ex:
+        if p == '(':
+            stack.push(p)
+        else:
+            if stack.pop() != '(':
+                b = False
+                break
+    if stack.pop() != -1:
+        b = False
+    if b:
+        print('YES')
+    else:
+        print('NO')
