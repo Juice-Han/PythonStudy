d8b58ebcb151695baa811619a9c843e03510e480
JuHan Park <iiiamst16@gmail.com>
2024-02-15 18:14:48 +0900

    DRF mixins & generics, viewset & router

--- django_rest/example_app/urls.py
+++ django_rest/example_app/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from .views import HelloAPI, bookAPI, booksAPI, BookAPI, BooksAPI
+from .views import HelloAPI, bookAPI, booksAPI, BookAPI, BooksAPI, BookAPIMixins, BooksAPIMixins, BooksAPIGenerics, \
+    BookAPIGenerics, BookViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
 
 urlpatterns = [
     path('hello/', HelloAPI),
-    path('fbv/book/', booksAPI),
+    path('fbv/books/', booksAPI),
     path('fbv/book/<int:bid>/', bookAPI),
-    path('cbv/book/', BooksAPI.as_view()),
+    path('cbv/books/', BooksAPI.as_view()),
     path('cbv/book/<int:bid>/', BookAPI.as_view()),
+    path('mixin/books/', BooksAPIMixins.as_view()),
+    path('mixin/book/<int:bid>', BookAPIMixins.as_view()),
+    path('generic/books/', BooksAPIGenerics.as_view()),
+    path('generic/book/<int:bid>', BookAPIGenerics.as_view()),
 ]
+
+urlpatterns += router.urls
