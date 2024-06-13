88dee3c2445b6d9287867282f702ef7e88229a5d
JuHan Park <iiiamst16@gmail.com>
2024-06-13 22:16:49 +0900

    31403번 A + B - C

---
+++ Python Algorithm solving/solved_ac/Class1.py
@@ -0,0 +1,7 @@
+# 31403번 A + B - C
+# from sys import stdin, stdout
+# numList = [0,0,0]
+# for i in range(3):
+#     numList[i] = int(stdin.readline())
+# stdout.write(str(numList[0]+numList[1]-numList[2])+str('\n'))
+# stdout.write(str(int(str(numList[0])+str(numList[1]))-numList[2]) + str('\n'))
